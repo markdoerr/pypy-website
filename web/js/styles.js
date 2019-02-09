ed8d98c70d78dc0bf26cce2d35d81cc0d86b8615
Duncan <frsdun@gmail.com>
2019-02-09 16:07:49 +0200

    Working on top navbar

--- web/js/styles.js
+++ web/js/styles.js
@@ -3,19 +3,22 @@
 // Navbar
 (function () {
     // Show or hide the navbar menu when icon is clicked
-    var navbars = document.getElementsByClassName("js-navbar");
+    function setupNavBar() {
+        console.log('here')
+        var navbars = document.getElementsByClassName("js-navbar");
+        [].forEach.call(navbars, function (navbar) {
+            var menuButtons = navbar.getElementsByClassName("js-navbar-toggler");
 
-    [].forEach.call(navbars, function (navbar) {
-        var menuButtons = navbar.getElementsByClassName("js-navbar-toggler");
+            [].forEach.call(menuButtons, function (menuButton) {
+                menuButton.addEventListener('click', function (e) {
+                    //console.log('click: toggle navbar menu')
+                    e.preventDefault();
+                    var related_navbar = navbar;
+                    related_navbar.classList.toggle('navbar-open');
+                });
 
-        [].forEach.call(menuButtons, function (menuButton) {
-            menuButton.addEventListener('click', function (e) {
-                //console.log('click: toggle navbar menu')
-                e.preventDefault();
-                var related_navbar = navbar;
-                related_navbar.classList.toggle('navbar-open');
             });
-
         });
-    });
+    }
+    document.addEventListener("DOMContentLoaded", setupNavBar);
 })();
