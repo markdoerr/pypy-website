5cf7c93607f8127887f3b8675c4062044e647403
Duncan <frsdun@gmail.com>
2019-02-10 11:08:34 +0200

    Better navbar that can work with no js at all

--- web/js/styles.js
+++ web/js/styles.js
@@ -2,23 +2,42 @@
 
 // Navbar
 (function () {
-    // Show or hide the navbar menu when icon is clicked
-    function setupNavBar() {
-        console.log('here')
+    // Swap simple navbar for smart navbar
+    function swapNavbars() {
         var navbars = document.getElementsByClassName("js-navbar");
         [].forEach.call(navbars, function (navbar) {
-            var menuButtons = navbar.getElementsByClassName("js-navbar-toggler");
-
-            [].forEach.call(menuButtons, function (menuButton) {
-                menuButton.addEventListener('click', function (e) {
-                    //console.log('click: toggle navbar menu')
-                    e.preventDefault();
-                    var related_navbar = navbar;
-                    related_navbar.classList.toggle('navbar-open');
+            if (navbar.classList.contains("navbar")) {
+                navbar.classList.remove("navbar");
+                navbar.classList.add("navbar-smart");
+                // also turn on the menu button
+                var menuButtons = navbar.parentNode.getElementsByClassName("js-navbar-toggler");
+                [].forEach.call(menuButtons, function (menuButton) {
+                    menuButton.classList.add("navbar-menu-on");
                 });
-
-            });
+            }
         });
     }
-    document.addEventListener("DOMContentLoaded", setupNavBar);
+
+    // Add event to show or hide the navbar menu when icon is clicked
+    function toggleNavbarMenu() {
+        var navbars = document.getElementsByClassName("js-navbar");
+
+        [].forEach.call(navbars, function (navbar) {
+            // only works with "navbar-smart"
+            if (navbar.classList.contains("navbar-smart")) {
+                var menuButtons = navbar.parentNode.getElementsByClassName("js-navbar-toggler");
+                [].forEach.call(menuButtons, function (menuButton) {
+                    menuButton.addEventListener('click', function (e) {
+                        e.preventDefault();
+                        var related_navbar = navbar;
+                        related_navbar.classList.toggle('navbar-open');
+                    });
+                });
+            }
+        });
+    }
+    document.addEventListener("DOMContentLoaded", function () {
+        swapNavbars();
+        toggleNavbarMenu();
+    });
 })();
